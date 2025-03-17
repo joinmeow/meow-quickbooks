c1309dfc447a9847b966ce8c1e55fc945f984963
Robert Joseph Wayne <robertjwayne@gmail.com>
2025-03-18 01:11:18 +0700

    remove trailing slash

--- quickbooks/client.py
+++ quickbooks/client.py
@@ -266,7 +266,9 @@ class QuickBooks(object):
             request_type, url, headers=headers, params=params, data=data)
 
     def get_single_object(self, qbbo, pk):
-        url = "{0}/company/{1}/{2}/{3}/".format(self.api_url, self.company_id, qbbo.lower(), pk)
+        url = "{0}/company/{1}/{2}/{3}".format(
+            self.api_url, self.company_id, qbbo.lower(), pk
+        )
 
         print(f"url: {url}")
 
